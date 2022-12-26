b312c28a5510a58508aa9b48a54ce62032aaa922
danielmrocha <rochadm@gmail.com>
2022-12-26 14:57:04 -0300

    Gulp file compile sass

---
+++ themes/gavias_kama/gulpfile.js
@@ -0,0 +1,55 @@
+let gulp = require('gulp'),
+  sass = require('gulp-sass')(require('sass')),
+  sourcemaps = require('gulp-sourcemaps'),
+  cleanCss = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  browserSync = require('browser-sync').create()
+
+const paths = {
+  scss: {
+    src: './sass/template.scss',
+    dest: './css',
+    watch: './scss/**/*.scss'
+  }
+}
+
+// Compile sass into CSS & auto-inject into browsers
+function styles () {
+  return gulp.src([paths.scss.src])
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer({
+      browsers: [
+        'Chrome >= 35',
+        'Firefox >= 38',
+        'Edge >= 12',
+        'Explorer >= 10',
+        'iOS >= 8',
+        'Safari >= 8',
+        'Android 2.3',
+        'Android >= 4',
+        'Opera >= 12']
+    })]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(cleanCss())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.scss.dest))
+}
+
+// Move the javascript files into our js folder
+
+// Static Server + watching scss/html files
+function watch () {
+  gulp.watch([paths.scss.watch], styles);
+}
+
+const build = gulp.parallel(styles);
+
+exports.styles = styles
+
+exports.watch = watch
+
+exports.default = build
